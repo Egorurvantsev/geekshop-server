209d4ef94a5a85b7e9d4c35c3b6772738c5fd232
Egor <egor.urvancev123@gmail.com>
2022-02-15 22:21:23 +0500

    added model Users

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -133,3 +133,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Users
+AUTH_USER_MODEL = 'users.User'
