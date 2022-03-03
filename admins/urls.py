3db6ff4dca49ccad245eac04a7f792d801a93af2
Egor <egor.urvancev123@gmail.com>
2022-03-03 17:17:22 +0500

    added updateview

--- admins/urls.py
+++ admins/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from admins.views import UserAdminListViews, UserAdminCreateViews, admin_users_update, admin_users_delete#, index
+from admins.views import UserAdminListView, UserAdminCreateView, UserAdminUpdateView, admin_users_delete#, index
 
 app_name = 'admins'
 
 urlpatterns = [
     #path('', index, name='index'),
-    path('users/', UserAdminListViews.as_view(), name='admin_users'),
-    path('users-create/', UserAdminCreateViews.as_view(), name='admin_users_create'),
-    path('users-update/<int:pk>/', admin_users_update, name='admin_users_update'),
+    path('users/', UserAdminListView.as_view(), name='admin_users'),
+    path('users-create/', UserAdminCreateView.as_view(), name='admin_users_create'),
+    path('users-update/<int:pk>/', UserAdminUpdateView.as_view(), name='admin_users_update'),
     path('users-delete/<int:pk>/', admin_users_delete, name='admin_users_delete'),
 ]
--- admins/views.py
+++ admins/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse, reverse_lazy
 from django.contrib import messages
 from django.contrib.auth.decorators import user_passes_test
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
 
 from users.models import User
@@ -11,13 +11,13 @@ from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
 
 
 #Read
-class UserAdminListViews(ListView):
+class UserAdminListView(ListView):
     model = User
     template_name = 'admins/admin-users-read.html'
 
 
 #Create
-class UserAdminCreateViews(CreateView):
+class UserAdminCreateView(CreateView):
     model = User
     template_name ='admins/admin-users-create.html'
     form_class = UserAdminRegistrationForm
@@ -25,24 +25,15 @@ class UserAdminCreateViews(CreateView):
 
 
 #Update
-@user_passes_test(lambda u: u.is_staff)
-def admin_users_update(request, pk):
-    selected_user = User.objects.get(id=pk)
-    if request.method == 'POST':
-        form = UserAdminProfileForm(instance=selected_user, files=request.FILES, data=request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Пользователь успешно редактирован.')
-            return HttpResponseRedirect(reverse('admin_staff:admin_users'))
-        else:
-            print(form.errors)
-    else:
-        form = UserAdminProfileForm(instance=selected_user)
+class UserAdminUpdateView(UpdateView):
+    model = User
+    template_name = 'admins/admin-users-update-delete.html'
+    success_url = reverse_lazy('admin_staff:admin_users')
+    form_class = UserAdminProfileForm
 
-    context = {
-        'title': 'GeekShop - Admin', 'form': form, 'selected_user': selected_user
-    }
-    return render(request, 'admins/admin-users-update-delete.html', context)
+
+# class UserAdminDeleteView()
+
 
 #Delete
 @user_passes_test(lambda u: u.is_staff)
@@ -90,3 +81,24 @@ def admin_users_delete(request, pk):
 #         'form': form
 #     }
 #     return render(request, 'admins/admin-users-create.html', context)
+
+
+#Update
+# @user_passes_test(lambda u: u.is_staff)
+# def admin_users_update(request, pk):
+#     selected_user = User.objects.get(id=pk)
+#     if request.method == 'POST':
+#         form = UserAdminProfileForm(instance=selected_user, files=request.FILES, data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, 'Пользователь успешно редактирован.')
+#             return HttpResponseRedirect(reverse('admin_staff:admin_users'))
+#         else:
+#             print(form.errors)
+#     else:
+#         form = UserAdminProfileForm(instance=selected_user)
+#
+#     context = {
+#         'title': 'GeekShop - Admin', 'form': form, 'selected_user': selected_user
+#     }
+#     return render(request, 'admins/admin-users-update-delete.html', context)
